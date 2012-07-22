79f0a63c1a7a4341ab5079a47b881115161e6c1b
i11016 <i11016@yuge.ac.jp>
2012-07-22 16:15:46 +0900

    WindowsExplorer風のファイルビュワーの試作機完成

--- C#/WindowsExplorer_Sample/WindowsExplorer/MainViewModel.cs
+++ C#/WindowsExplorer_Sample/WindowsExplorer/MainViewModel.cs
@@ -8,52 +8,48 @@ using System.ComponentModel;
 
 namespace WindowsExplorer
 {
-    class MainViewModel
+    class MainViewModel : INotifyPropertyChanged
     {
         public MainViewModel()
-        {
-            ObservableCollection<File> files = new ObservableCollection<File>();
-            for (int i = 0; i < 10; i++)
-            {
-                files.Add(new File() { Name = "Image.jpg", ImageSize = "256 × 256", Type = "JPEG イメージ", Size = "256 KB", CreateDate = "2011/11/11 11:11" });
-            }
-            Files = files;
-            SelectedFile = files[0];
-        }
-
-            public ObservableCollection<File> Files { get; set; }
-            
-            private File selectedFile;
-            
-            public File SelectedFile
-           {
-                get
-                {
-                    return selectedFile;
-                }
-                set
-                {
-                    selectedFile = value;
-                    //  NotifyPropertyChanged("SelectedFile");
-                    NotifyPropertyChanged("SelectedFile");
-                }
-            }
-
-               #region INotifyPropertyChanged
-
-            public event PropertyChangedEventHandler PropertyChanged;
-
-         private void NotifyPropertyChanged(String info)
-           { 
-            
-             if (PropertyChanged != null)
-             {
-                PropertyChanged(this, new PropertyChangedEventArgs(info));
-             }
-
-           }
-               #endregion
-
-        }
+        { 
+                 ObservableCollection<File> files = new ObservableCollection<File>();
+      for (int i = 0; i < 10; i++)
+      {
+        files.Add(new File() { Name = "Image.jpg", ImageSize = "256 × 256", Type = "JPEG イメージ", Size = "256 KB", CreateDate = "2011/11/11 11:11" });
+      }
+      Files = files;
+      SelectedFile = files[0];
     }
+
+    public ObservableCollection<File> Files { get; set; }
+    private File selectedFile;
+    public File SelectedFile
+    {
+      get
+      {
+        return selectedFile;
+      }
+      set
+      {
+        selectedFile = value;
+        NotifyPropertyChanged("SelectedFile");
+      }
+    }
+
+    #region INotifyPropertyChanged
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void NotifyPropertyChanged(String info)
+    {
+      if (PropertyChanged != null)
+      {
+        PropertyChanged(this, new PropertyChangedEventArgs(info));
+      }
+    }
+    #endregion
+        
+    }
+    
+}
 
